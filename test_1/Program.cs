50f215c5fbca5c9cf6b9b8a64c457263197696de
Bozheniu <bozheniu@gmail.com>
2022-10-20 11:25:44 +0200

    Добавила решение задания номер 2 в файл test_1

---
+++ test_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа 
+и выдаёт, какое число большее, а какое меньшее.
+*/
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+bool isNotNumberAEqualB = a != b;
+
+if (isNotNumberAEqualB)
+{
+
+    if (a > b && isNotNumberAEqualB)
+    {
+        Console.WriteLine($"Число a = {a} = max");
+    }
+    else
+    {
+        Console.WriteLine($"Число b = {b} = max");
+    }
+}
+else
+{
+    Console.WriteLine($"Число a = {a} равняется числу b = {b}");
+}
