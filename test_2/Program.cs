5ba76ce410b347d61dba80681097562578f50e89
Bozheniu <bozheniu@gmail.com>
2022-10-22 01:45:29 +0200

    Исправила задание номер 4

--- test_2/Program.cs
+++ test_2/Program.cs
@@ -10,18 +10,8 @@ int b = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третье число: ");
 int c = int.Parse(Console.ReadLine()!);
 
-if (a > b && a > c && a != b && a != c)
-{
-    Console.WriteLine($" Максимальное число равняется a = {a} = max");
-}
-else
-{
-    if (b > c && b != c)
-    {
-        Console.WriteLine($"Максимальное число равняется b = {b} = max");
-    }
-    else
-    {
-        Console.WriteLine($"Максимальное число равняется c = {c} = max ");
-    }
-}
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine($"max = {max}");
